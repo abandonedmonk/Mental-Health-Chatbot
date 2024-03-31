90049d557b7994c9860b15486bf1756ec6be32e2
Anshuman Jena <66255580+abandonedmonk@users.noreply.github.com>
2024-03-31 19:03:16 +0530

    Imported all the necessary libraries

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from streamlit_chat import message
+from langchain.chains import ConversationalRetrievalChain
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.llms import CTransformers
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
